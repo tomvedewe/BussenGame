90ab40479da4f80b2b80c80edc304e5f5dc4654f
Tom <tom.vanderweeen.w4095@student.hogent.be>
2021-07-23 16:30:12 +0200

    Initial Commit

---
+++ Bussen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bussen
+//
+//  Created by Tom Van der Weeën on 23/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
